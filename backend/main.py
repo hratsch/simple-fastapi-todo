be6f755e776e2cf32183bac06bf8c1d35faaad6a
Hugh <hugh.ratsch.18@gmail.com>
2025-11-03 17:01:47 -0600

    Restructure: Isolate backend/ folder

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # Svelte dev port 
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 class Todo(BaseModel):
     title: str
